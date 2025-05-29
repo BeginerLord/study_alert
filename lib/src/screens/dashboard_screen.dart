3f086e42a9fe41704f6b879963308c438aa1cb44
BeginerLord <messismark0@gmail.com>
2025-05-29 14:13:59 -0500

    Fix: add missing comment for private state class in DashboardScreen

--- lib/src/screens/dashboard_screen.dart
+++ lib/src/screens/dashboard_screen.dart
@@ -7,6 +7,7 @@ class DashboardScreen extends StatefulWidget {
   const DashboardScreen({super.key, required this.userCedula});
 
   @override
+  // ignore: library_private_types_in_public_api
   _DashboardScreenState createState() => _DashboardScreenState();
 }
 
